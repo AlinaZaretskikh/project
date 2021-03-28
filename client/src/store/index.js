d7c4c6b1986531126673ec0d77469c8c428213d5
AlinaZaretskikh <alinazaretskikh12@gmail.com>
2021-03-28 21:21:49 +0300

    1

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,13 +2,15 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import users from './modules/users'
 import products from './modules/products'
+import categories from './modules/categories'
 
 Vue.use(Vuex);
 
 let store = new Vuex.Store({
     modules: {
         users,
-        products
+        products,
+        categories
     }
 })
 
