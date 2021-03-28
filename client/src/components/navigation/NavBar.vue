752adab25ce33dafc0a06a21c372bfbeccd5bc64
seranerra <72248201+seranerra@users.noreply.github.com>
2021-03-28 13:14:17 +0700

    3

--- client/src/components/navigation/NavBar.vue
+++ client/src/components/navigation/NavBar.vue
@@ -1,39 +1,23 @@
 <template>
     <div class="nav-bar">
         <div class="left-nav-bar">
-            <nav>
-                <ul>
-                    <li>
-                        <a href="">Главное</a>
-                    </li>
-                    <li>
-                        <a href="">Каталог</a>
-                    </li>
-                    <li>
-                        <a href="">Корзина</a>
-                    </li>
-                </ul>
-            </nav>
+        <ul>
+            <li><a href="#">Главная</a></li>
+            <li><a href="#">Каталог</a></li>
+            <li><a href="#">Корзина</a></li>
+        </ul>
         </div>
 
         <div class="center-nav-bar">
-            <p>ЛОГОТИП</p>
+            <ul><li>LOGOTIP</li></ul>
         </div>
 
         <div class="right-nav-bar">
-            <nav>
-                <ul>
-                    <li>
-                        <a href="">Избранное</a>
-                    </li>
-                    <li>
-                        <a href="">Помощь</a>
-                    </li>
-                    <li>
-                        <a href="">Вход</a>
-                    </li>
-                </ul>
-            </nav>
+        <ul>
+            <li><a href="#">Избранное</a></li>
+            <li><a href="#">Помощь</a></li>
+            <li><a href="#">Вход</a></li>
+        </ul>
         </div>
     </div>
 </template>
@@ -47,20 +31,59 @@ export default {
 }
 </script>
 
+
 <style scoped>
-.nav-bar {
+/* .nav-bar {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    background-color: yellow;
+    background-color: blue;
+} */
+.nav-bar {
+    display: inline;
 }
-ul {
+.left-nav-bar {
+    display: inline-block;
+    background-color: brown;
+}
+.right-nav-bar {
+     display: inline-block;
+    background-color: burlywood;
+}
+.center-nav-bar {
+     display: inline-block;
+    background-color: darkblue;
+}
+/* ul {
     display: flex;
     flex-direction: row;
+} */
+
+ul {
+    margin-left: 0px;
+    padding-left: 0px;
+    list-style: none;
 }
-li {
+/* li {
     width: 150px;
     list-style: none;
-    border: solid 1px silver;
+    border: solid 3px black;
+} */
+li { 
+    display: inline; 
 }
+ul a, .center-nav-bar {
+                display: inline-block;
+                width: 5em;
+                padding:10px;
+                background-color: #f4f4f4;
+                border: 1px solid #333;
+                text-decoration: none;
+                color: #333;
+                text-align: center;
+            }
+ul a:hover{
+                background-color: #333;
+                color: #f4f4f4;
+            }
 </style>
