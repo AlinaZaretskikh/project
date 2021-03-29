630b91796c3041a8a860a19d0a084b2e166dce98
seranerra <72248201+seranerra@users.noreply.github.com>
2021-03-29 14:44:16 +0700

    1

---
+++ client/src/components/navigation/Navigation.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="navigation">
+        <div>
+            <p>khvhvh</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navigation'
+}
+</script>
+
+<style>
+
+</style>
--- client/src/components/pages/MainPage.vue
+++ client/src/components/pages/MainPage.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="main-page">
+        <Navigation/>
         <NavBar/>
         <ProductCard/>        
     </div>
 </template>
 
 <script>
+import Navigation from '../navigation/Navigation.vue'
 import NavBar from '../navigation/NavBar.vue'
 import ProductCard from '../cards/ProductCard.vue'
 export default {
     name: 'MainPage',
     components: {
+        Navigation,
         NavBar,
         ProductCard
     }
